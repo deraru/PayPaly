ebb2de4b7487889d2eba0e65a67ad472b96359a1
miata <desktopcorps@gmail.com>
2012-04-28 04:24:18 +0900

    skelton class

---
+++ paypaly.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Yii Framework Extension for PayPal Express Checkout
+ *
+ * @author miata
+ */
+class PayPaly
+{
+	const API_VERSION = '88.0';
+	const API_ENDPOINT = 'https://api-3t.paypal.com/nvp';
+	const API_ENDPOINT_SANDBOX = 'https://api-3t.sandbox.paypal.com/nvp';
+
+	public function setupCheckout()
+	{
+
+	}
+
+	public function getCheckoutDetail()
+	{
+
+	}
+
+	public function doCheckout()
+	{
+
+	}
+
+	public function refund()
+	{
+
+	}
+
+	private function request($mehtod, $param)
+	{
+
+	}
+}
